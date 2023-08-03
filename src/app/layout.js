ef420f2b447a6fa015db963f07f49e2a704efb7e
Maltonn <ymatsubayashi09@outlook.jp>
2023-08-03 17:20:45 +0900

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-'use client'
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+// import { useState } from 'react'
+// import { Dialog } from '@headlessui/react'
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -19,7 +18,7 @@ export default function RootLayout({ children }) {
     { name: 'about', href: '/about' },
     { name: 'Links', href: '/links' },
   ]
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <html lang="ja">
@@ -37,7 +36,7 @@ export default function RootLayout({ children }) {
                   />
                 </a>
               </div>
-              <div className="flex lg:hidden">
+              {/* <div className="flex lg:hidden">
                 <button
                   type="button"
                   className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -46,7 +45,7 @@ export default function RootLayout({ children }) {
                   <span className="sr-only">Open main menu</span>
                   <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                 </button>
-              </div>
+              </div> */}
               <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                 {navigation.map((item) => (
                   <a
@@ -60,7 +59,7 @@ export default function RootLayout({ children }) {
 
               </div>
             </nav>
-            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+            {/* <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
               <div className="fixed inset-0 z-50" />
               <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
@@ -105,7 +104,7 @@ export default function RootLayout({ children }) {
                   </div>
                 </div>
               </Dialog.Panel>
-            </Dialog>
+            </Dialog> */}
           </header>
           {children}
         </div>
